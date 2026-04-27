565d8a5941c71c54cc2b8a9fe4f0c99657783a28
Biraj Kashyap <kashyapbiraj83@gmail.com>
2026-04-27 23:21:28 +0530

    feat: socketio client added

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,11 +1,34 @@
-import "./App.css";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:4000", { autoConnect: false });
 
-function App() {
-  return (
-    <>
-      <div>Hello world</div>
-    </>
-  );
+import { useState, useEffect } from "react";
+
+export default function ChatApp() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    socket.connect();
+
+    function onConnect() {
+      setIsConnected(true);
+    }
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = (msg) => {
+    socket.emit("chat message", msg);
+  };
+
+  return <div>Status: {isConnected ? "Connected" : "Disconnected"}</div>;
 }
-
-export default App;
